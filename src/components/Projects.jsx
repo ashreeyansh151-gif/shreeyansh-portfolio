2c01b62400c489105a4320b00e89b97ffbddda50
Shreeyansh Suman Acharya <ashreeyansh151@gmail.com>
2026-07-13 14:57:02 +0530

    a

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,44 +1,47 @@
 import portfolio from "../assets/images/portfolio.png";
+
 function Projects() {
- const Projects = [
-  {
-    title: "Personal Portfolio",
-    image: portfolio,
-    description:
-      "A responsive portfolio website built with React, Vite and CSS showcasing my cricket journey, skills, projects and achievements.",
-    tech: "React • Vite • CSS",
-    live: "https://shreeyansh-portfolio-coral.vercel.app/",
-    github: "https://github.com/ashreeyansh151-gif/shreeyansh-portfolio",
-  },
-];
+  const projects = [
+    {
+      title: "Personal Portfolio",
+      image: portfolio,
+      description:
+        "A responsive portfolio website built with React, Vite and CSS showcasing my cricket journey, skills, projects and achievements.",
+      tech: "React • Vite • CSS",
+      live: "https://shreeyansh-portfolio-coral.vercel.app/",
+      github: "https://github.com/ashreeyansh151-gif/shreeyansh-portfolio",
+    },
+  ];
 
   return (
-    <section className="Projects" id="👨‍💻 Projects">
+    <section className="Projects" id="projects">
       <h2>👨‍💻 Projects</h2>
+
       <p className="Projects-subtitle">
         Some of the projects I have built during my learning journey.
       </p>
 
       <div className="Projects-container">
-        {Projects.map((Project, index) => (
+        {projects.map((project, index) => (
           <div className="Project-card" key={index}>
-           <img
-  src={project.image}
-  alt={project.title}
-  className="project-image"
-/>
-            <h3>{Project.title}</h3>
+            <img
+              src={project.image}
+              alt={project.title}
+              className="Project-image"
+            />
 
-            <p>{Project.description}</p>
+            <h3>{project.title}</h3>
 
-            <span>{Project.tech}</span>
+            <p>{project.description}</p>
+
+            <span>{project.tech}</span>
 
             <div className="project-buttons">
-              <a href={Project.live} target="_blank" rel="noreferrer">
+              <a href={project.live} target="_blank" rel="noreferrer">
                 🌐 Live Demo
               </a>
 
-              <a href={Project.github} target="_blank" rel="noreferrer">
+              <a href={project.github} target="_blank" rel="noreferrer">
                 💻 GitHub
               </a>
             </div>
