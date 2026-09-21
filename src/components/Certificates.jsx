95bad98dedfca2879247d3c0d670ced91da97ea8
Shreeyansh Suman Acharya <ashreeyansh151@gmail.com>
2026-09-21 11:30:04 +0530

    cert

--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -4,12 +4,14 @@ import Gen_AI from "../assets/images/Gen_AI.png";
 
 function Certificates() {
   return (
-    <section className="Certificates" id="📜Certificates">
-      <h2>📜Certificates</h2>
+    <section className="Certificates" id="Certificates">
+      <h2>📜 Certificates</h2>
 
       <div className="Certificates-container">
 
         <div className="Certificates-card">
+          <img src={cert1} alt="School National Cricket Certificate" />
+           <img src={cert2} alt="School National Cricket Certificate 2" />
           <h3>🏏 School National Cricket</h3>
           <p>Captain – 3 Times</p>
         </div>
@@ -20,8 +22,9 @@ function Certificates() {
         </div>
 
         <div className="Certificates-card">
-          <h3>🎓 Academic Achievement</h3>
-          <p>Rank 3 in Class 12</p>
+          <img src={Gen_AI} alt="Generative AI Certificate" />
+          <h3>🤖 Generative AI</h3>
+          <p>Generative AI Certification</p>
         </div>
 
       </div>
